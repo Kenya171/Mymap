010f0cee13d7a24ef9d0c4fa81bb71fbe37148f1
Kenya171 <fujikawa.k569@gmail.com>
2023-03-09 22:19:11 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 藤川賢也 on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
