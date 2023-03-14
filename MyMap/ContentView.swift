eecf0ea8a099e2760624e9e5caae0f980ea6abea
Kenya171 <fujikawa.k569@gmail.com>
2023-03-14 22:57:22 +0900

    Mapアプリ

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    入力中の文字列を保持する状態
+    @State var inputText: String = ""
+//    検索キーワードを保持する状態変数
+    @State var displaySearchKey: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+//            テキストフィールド（文字入力）
+            TextField("キーワード",text: $inputText, prompt: Text("キーワードを入力してください"))
+            
+//            入力が完了されたとき
+                .onSubmit {
+//                入力が完了したので検索キーワードに設定する
+                displaySearchKey = inputText
+            }
+//            余白を追加
+            .padding()
+//            マップを表示
+            MapView(searchKey: displaySearchKey)
         }
-        .padding()
     }
 }
 
